20e4538e463c3f21167be1c601ec6f95a45c42d4
vunh <vunh@greenglobal.vn>
2017-10-05 19:21:00 +0700

    add function create

--- app/Product.php
+++ app/Product.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Product extends Model
 {
 	protected $table = 'products';
+
+	public function save($data)
+	{
+		return Product::save($data);
+	}
 }
